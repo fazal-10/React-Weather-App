d9e43ea4e18f744a387f7a5b41fe3c47e6d4bc17
fazal-10 <fazalmansuri08@gmail.com>
2023-07-31 19:02:14 +0530

    files added

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a21d133bcfmsh2ed04211264b43cp1ae9e5jsn6e29e3f01f0e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0d355e4b32708dd2ca9bb445cfadb3cd";
